47c3db1f02cc5c142936bb9b70b9c864f14e51bd
Sam Goddard <hello@samgoddard.co.uk>
2020-06-18 13:25:11 +0100

    first pass at some anims

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
 import { motion } from "framer-motion"
 import { Link } from "gatsby"
 import Arrow from "../images/arrow.inline.svg";
-import gsap from "gsap";
+import gsap, { Power3 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 if (typeof window !== `undefined`) {
@@ -78,6 +79,51 @@ class IndexPage extends React.Component {
       return <div className="reveal-right" ref={revealRef}>{children}</div>;
     };
 
+    const ImageReveal = ({ children }) => {
+      const revealRef = useRef(null);
+
+      useEffect(() => {
+        gsap.to(revealRef.current, {
+          scrollTrigger: revealRef.current,
+          y: '-100%',
+          ease: Power3.easeInOut,
+          duration: 2
+        });
+      }, []);
+    
+      return <div className="reveal-image z-20 h-full bg-purple absolute bottom-0 left-0 right-0" ref={revealRef}>{children}</div>;
+    };
+
+    const GradientRevealRight = ({ children }) => {
+      const revealRef = useRef(null);
+
+      useEffect(() => {
+        gsap.to(revealRef.current, {
+          scrollTrigger: revealRef.current,
+          width: '33.33%',
+          ease: Power3.easeInOut,
+          duration: 2
+        });
+      }, []);
+    
+      return <div className="w-0 h-full gradient-ltr absolute top-0 right-0 z-0 hidden md:block" ref={revealRef}>{children}</div>;
+    };
+    
+    const GradientRevealLeft = ({ children }) => {
+      const revealRef = useRef(null);
+
+      useEffect(() => {
+        gsap.to(revealRef.current, {
+          scrollTrigger: revealRef.current,
+          width: '33.33%',
+          ease: Power3.easeInOut,
+          duration: 2
+        });
+      }, []);
+    
+      return <div className="w-0 h-full gradient-ltr absolute top-0 left-0 z-0 hidden md:block" ref={revealRef}>{children}</div>;
+    };
+
     return (
       <>
         <SEO title="Home" />
@@ -88,11 +134,19 @@ class IndexPage extends React.Component {
           className="bg-purple pt-8 pb-24 md:pt-12 md:pb-24 xl:pt-24"
         >
           <motion.div 
-            className="container lg:px-32"
+            className="absolute top-0 left-0 w-full z-0 overflow-hidden w-full"
             variants={item}
             transition="easeInOut"
           >
-            <div className="w-11/12 md:w-8/12">
+            <svg className="logo-bg opacity-25" viewBox="0 0 44 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.316 31.173l9.259-.004 3.326 5.685h10.283L21.59 0 16.4 8.866l7.972 13.487-9.404.013-6.617 11.235H5.726L15.044 17.7l1.402 2.386h3.827l-5.237-8.892L0 36.858h10.262l6.613-11.24 13.26.013-9.913-16.773 1.368-2.336 15.83 27.025h-2.63L31.445 27.9l-13.295.008-5.199 8.95h16.914l-1.912-3.244h-9.276l1.427-2.436.213-.005z" fill="url(#paint0_linear)"/><defs><linearGradient id="paint0_linear" x1="42.07" y1="9.562" x2="8.917" y2="40.813" gradientUnits="userSpaceOnUse"><stop stopColor="#D51F3A"/><stop offset="1" stopColor="#2C3583"/></linearGradient></defs></svg>
+          </motion.div>
+
+          <motion.div 
+            className="container lg:px-32 relative z-10"
+            variants={item}
+            transition="easeInOut"
+          >
+            <div className="w-11/12 md:w-9/12 lg:w-8/12">
               <h1 className="text-4xl md:text-6xl xl:text-7xl font-medium text-white mb-6 md:mb-8">Unleashing the digital leaders of tomorrow</h1>
 
               <div className="w-8/12 md:w-10/12">
@@ -114,11 +168,11 @@ class IndexPage extends React.Component {
           className="bg-purple"
         >
           <div className="flex flex-wrap">
-            <div className="w-full md:w-9/12 lg:w-8/12 ml-auto">
+            <div className="w-full md:w-9/12 lg:w-8/12 ml-auto relative z-10">
               <div className="gradient-ltr">
                 <div className="flex flex-wrap xl:w-10/12 p-4">
                   <div className="w-1/2">
-                    <Link to="/" className="block py-6 px-10">
+                    <Link to="/" className="block py-6 md:px-10 text-center md:text-left">
                       <span className="block text-white text-2xl md:text-3xl font-medium">Learners</span>
                       <div className="hidden md:block">
                         <span className="block text-white text-lg mb-3">Develop from learner into leader</span>
@@ -131,7 +185,7 @@ class IndexPage extends React.Component {
                   </div>
 
                   <div className="w-1/2 md:border-l md:border-white">
-                    <Link to="/" className="block py-6 px-10">
+                    <Link to="/" className="block py-6 md:px-10 text-center md:text-left">
                       <span className="block text-white text-2xl md:text-3xl font-medium">Employers</span>
                       <div className="hidden md:block">
                         <span className="block text-white text-lg mb-3">Find the talent you need</span>
@@ -153,7 +207,7 @@ class IndexPage extends React.Component {
           variants={container}
           initial="hidden" 
           animate="visible"
-          className="bg-purple pt-10 pb-24 md:pt-12 lg:pt-0 md:pb-32 overflow-hidden"
+          className="bg-purple pt-10 pb-24 mb-16 md:mb-0 md:pt-12 lg:pt-0 md:pb-32 overflow-hidden relative z-10"
         >
           <RevealLeft>
             <span className="text-8xl lg:text-9xl xl:text-10xl uppercase font-extrabold scroll-text text-transparent">Why Us?</span>
@@ -162,7 +216,11 @@ class IndexPage extends React.Component {
           <div className="container lg:px-32 -mt-16 lg:-mt-24 xl:-mt-32 relative z-20">
             <div className="flex flex-wrap md:-mx-8">
               <div className="w-full md:w-1/2 lg:w-7/12 md:px-8 mb-8 md:mb-0">
-                <img src="https://placedog.net/1200/700" alt="placeholder" />
+                <div className="relative overflow-hidden">
+                  <ImageReveal>
+                  </ImageReveal>
+                  <img src="https://placedog.net/1200/700" alt="placeholder" />
+                </div>
               </div>
               <div className="w-full md:w-1/2 lg:w-5/12 md:px-8 content md:pr-16 text-white">
                 <h2 className="pb-6 mb-0">About Us</h2>
@@ -188,16 +246,17 @@ class IndexPage extends React.Component {
           variants={container}
           initial="hidden" 
           animate="visible"
-          className="bg-white mt-10 pb-24 md:mt-24 md:pb-8 lg:pb-8 xl:mt-32 xl:pb-24 overflow-hidden relative"
+          className="bg-white mt-10 pb-12 md:mt-24 md:pb-8 lg:pb-8 xl:mt-32 xl:pb-24 overflow-hidden relative"
         >
           <RevealLeft>
             <span className="text-8xl lg:text-9xl xl:text-10xl uppercase font-extrabold scroll-text text-transparent leading-none -mt-4 xl:-mt-8 block">Alternative</span>
           </RevealLeft>
           
-          <div className="w-4/12 h-full gradient-ltr absolute top-0 right-0 z-0"></div>
+          <GradientRevealRight>
+          </GradientRevealRight>
           
-          <div className="container lg:px-32 -mt-10 md:-mt-20 xl:-mt-32 relative z-20">
-            <div className="flex flex-wrap md:-mx-8">
+          <div className="container lg:px-32 -mt-16 md:-mt-20 xl:-mt-32 relative z-20">
+            <div className="flex flex-wrap items md:-mx-8">
               <div className="w-full md:w-1/2 lg:w-5/12 md:px-8 content mb-8 md:mb-0 order-2 md:order-1 md:pt-24 lg:pt-32">
                 <h2 className="pb-6 mb-0">Learners</h2>
                 <span className="w-20 h-2 bg-pink block mb-8"></span>
@@ -215,8 +274,14 @@ class IndexPage extends React.Component {
                   <span className="block ml-4"><Arrow /></span>
                 </Link>
               </div>
-              <div className="w-full md:w-1/2 lg:w-7/12 md:px-8 order-1 md:order-2 pb-8 md:pb-0">
-                <img src="https://placedog.net/1200/700" alt="placeholder" />
+              <div className="w-full md:w-1/2 lg:w-7/12 md:px-8 order-1 md:order-2 pb-8 mb-8 md:mb-0 md:pb-0 relative">
+                <div className="w-7/12 h-full gradient-ltr absolute top-0 right-0 z-0 block md:hidden -mr-8"></div>
+                
+                <div className="relative overflow-hidden mt-10 mb-4">
+                  <ImageReveal>
+                  </ImageReveal>
+                  <img src="https://placedog.net/1200/700" alt="placeholder" className="relative z-10" />
+                </div>
               </div>
             </div>
           </div>
@@ -227,22 +292,27 @@ class IndexPage extends React.Component {
           variants={container}
           initial="hidden" 
           animate="visible"
-          className="bg-white mt-10 pb-24 mb-16 md:mt-24 md:mb-20 md:pb-8 lg:mb-32 xl:mb-40 lg:pb-8 xl:mt-32 xl:pb-24 overflow-hidden relative"
+          className="bg-white mt-10 pb-24 md:mt-24 md:pb-8 lg:pb-8 xl:mt-32 xl:pb-24 overflow-hidden relative"
         >
           <RevealRight>
             <span className="text-8xl lg:text-9xl xl:text-10xl uppercase font-extrabold scroll-text-right text-right text-transparent leading-none -mt-4 xl:-mt-8 block">Digital</span>
           </RevealRight>
           
-          <div className="w-4/12 h-full gradient-ltr absolute top-0 left-0 z-0"></div>
+          <GradientRevealLeft>
+          </GradientRevealLeft>
           
-          <div className="container lg:px-32 -mt-10 md:-mt-20 xl:-mt-32 relative z-20">
-            <div className="flex flex-wrap md:-mx-8">
-
-              <div className="w-full md:w-1/2 lg:w-7/12 md:px-8 pb-8 md:pb-0">
-                <img src="https://placedog.net/1200/700" alt="placeholder" />
+          <div className="container lg:px-32 -mt-16 md:-mt-20 xl:-mt-32 relative z-20">
+            <div className="flex flex-wrap items md:-mx-8">
+              <div className="w-full md:w-1/2 lg:w-7/12 md:px-8 pb-8 md:pb-0 relative mb-8 md:mb-0">
+                <div className="w-7/12 h-full gradient-ltr absolute top-0 left-0 z-0 block md:hidden -ml-8"></div>
+                <div className="relative overflow-hidden mt-10 mb-4">
+                  <ImageReveal>
+                  </ImageReveal>
+                  <img src="https://placedog.net/1200/700" alt="placeholder" className="relative z-10" />
+                </div>
               </div>
 
-              <div className="w-full md:w-1/2 lg:w-5/12 md:px-8 content mb-8 md:mb-0 md:pt-24 lg:pt-32">
+              <div className="w-full md:w-1/2 lg:w-5/12 md:px-8 content md:pt-24 lg:pt-32">
                 <h2 className="pb-6 mb-0">Employers</h2>
                 <span className="w-20 h-2 bg-pink block mb-8"></span>
 
@@ -255,7 +325,7 @@ class IndexPage extends React.Component {
                 </div>
 
                 <Link to="/" className="text-md text-pink font-semibold uppercase flex flex-wrap items-center block">
-                  <span className="block">Find out more</span>
+                  <span className="block">Find Out more</span>
                   <span className="block ml-4"><Arrow /></span>
                 </Link>
               </div>
@@ -263,14 +333,15 @@ class IndexPage extends React.Component {
           </div>
         </motion.section>
 
+
         <motion.section
           variants={container}
           initial="hidden" 
           animate="visible"
-          className="bg-white py-6 md:py-10 lg:py-16 xl:py-20"
+          className="bg-white pt-6 md:pt-10 lg:pt-16 xl:pt-20 overflow-hidden"
         >
           <div className="container lg:px-32">
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap pb-6 md:pb-10 lg:pb-16 xl:pb-20">
               <div className="w-full md:w-7/12 lg:w-5/12 ml-auto">
                 <span className="text-4xl md:text-5xl xl:text-6xl font-semibold leading-extra-tight block mb-10">Ready to start your journey?</span>
                 <Link to="/" className="text-md text-pink font-semibold uppercase flex flex-wrap items-center block">
@@ -280,7 +351,15 @@ class IndexPage extends React.Component {
               </div>
             </div>
           </div>
+          <div className="bg-purple-light opacity-75 w-full h-64">
+            <RevealLeft>
+              <span className="text-8xl lg:text-9xl xl:text-10xl uppercase font-extrabold scroll-text scroll-text--white text-transparent leading-none block pt-48 md:pt-48 lg:pt-40 xl:pt-20">Contact</span>
+            </RevealLeft>
+          </div>
+          <div className="bg-purple w-full h-20"></div>
         </motion.section>
+
+        <Footer />
       </>
     )
   }
