3be910771a227f4dfce019b464b800785c09b099
Sam Goddard <hello@samgoddard.co.uk>
2020-06-19 11:13:47 +0100

    further scaffolding

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,39 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { motion, AnimatePresence } from "framer-motion"
+// import { motion, AnimatePresence } from "framer-motion"
 import { Link } from "gatsby"
 
 import "../styles/main.css"
 
-const duration = 0.35
+// const duration = 0.35
 
-const variants = {
-  initial: {
-    opacity: 0,
-  },
-  enter: {
-    opacity: 1,
-    transition: {
-      duration: duration,
-      ease: [0.43, 0.13, 0.23, 0.96],
-      delay: duration,
-      when: 'beforeChildren',
-      staggerChildren: 0.5,
-    },
-  },
-  exit: {
-    opacity: 0,
-    transition: {
-      staggerChildren: 0.5,
-      ease: [0.43, 0.13, 0.23, 0.96],
-      duration: duration,
-    },
-  },
-}
+// const variants = {
+//   initial: {
+//     opacity: 0,
+//   },
+//   enter: {
+//     opacity: 1,
+//     transition: {
+//       duration: duration,
+//       ease: [0.43, 0.13, 0.23, 0.96],
+//       delay: duration,
+//       when: 'beforeChildren',
+//       staggerChildren: 0.5,
+//     },
+//   },
+//   exit: {
+//     opacity: 0,
+//     transition: {
+//       staggerChildren: 0.5,
+//       ease: [0.43, 0.13, 0.23, 0.96],
+//       duration: duration,
+//     },
+//   },
+// }
 
 const Layout = ({ children, location }) => {
   return (
     <>
-      {/* Fixed text blocks */}
       <div className="fixed z-20 fixed-text-left pl-6 hidden lg:block">
         <span className="text-lg text-white">Learn Digital. Lead Digital.</span>
       </div>
@@ -44,17 +43,9 @@ const Layout = ({ children, location }) => {
           <Link to="/" className="xl:text-lg text-white bg-pink p-3 py-5 uppercase font-semibold mt-3">Employers</Link>
         </div>
       </div>
-        <AnimatePresence exitBeforeEnter>
-          <motion.main
-            key={location.pathname}
-            variants={variants}
-            initial="initial"
-            animate="enter"
-            exit="exit"
-          >
+        <main>
           {children}
-          </motion.main>
-        </AnimatePresence>
+        </main>
     </>
   )
 }
