7a90670493a227c225a46d516f3773c8b8feac34
Sam Goddard <hello@samgoddard.co.uk>
2020-07-06 15:23:25 +0100

    smooth scroll reenabled on mobile

--- src/components/SmoothScroll.js
+++ src/components/SmoothScroll.js
@@ -23,35 +23,37 @@ export default class SmoothScroll extends React.Component {
   });
 
   componentDidMount() {
-    if (!isMobile().any) {
+    // if (!isMobile().any) {
       windowGlobal.addEventListener("scroll", this.onScroll);
       this.ro.observe(this.viewport);
-    }
+    // }
   }
 
   onScroll = () => {
-    if (!isMobile().any) {
+    // if (!isMobile().any) {
       TweenLite.to(this.viewport, 1, {
         y: -windowGlobal.pageYOffset,
         ease: Power2.easeOut
       });
-    }
+    // }
   };
 
   render() {
     return (
       <>
-        <div className={ isMobile().any ? `` : `viewport`} ref={ref => (this.viewport = ref)}>
+      {/* { !isMobile().any && ( */}
+        <div className="viewport" ref={ref => (this.viewport = ref)}>
           {this.props.children}
         </div>
-        { !isMobile().any && (
+      {/* )} */}
+        {/* { !isMobile().any && ( */}
           <div
             ref={ref => (this.fake = ref)}
             style={{
               height: this.state.height
             }}
           />
-        )}
+        {/* )} */}
       </>
     );
   }
