840bcaa5d5c00503c47a5a551d9315628b9e3c70
Sam Goddard <hello@samgoddard.co.uk>
2020-09-27 14:58:44 +0100

    added footer accreditations from client

--- src/components/seo.js
+++ src/components/seo.js
@@ -44,7 +44,7 @@ const SEO = ({ pathname, titleOverride, descriptionOverride, pathnameOverride, i
         }
       }
     }
-  `)  
+  `)
   return (
     <Helmet defer={false} titleTemplate={`%s${titleSuffix}`}>
       <html lang="en" />
@@ -60,7 +60,7 @@ const SEO = ({ pathname, titleOverride, descriptionOverride, pathnameOverride, i
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en" />
       <meta property="og:site_name" content={siteName} />
-      <meta property="og:image" content={`${imageOverride ? imageOverride : url}`} />
+      <meta property="og:image" content={`${imageOverride ? imageOverride : 'https://www.datocms-assets.com/30296/1593166456-mission-2x.png'}`} />
       <meta property="og:image:width" content="1920" />
       <meta property="og:image:height" content="1080" />
 
